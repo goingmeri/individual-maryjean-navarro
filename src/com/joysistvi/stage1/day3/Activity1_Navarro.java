6280c332a0a180b4e8fc244994e8468377dbd4ee
Mary Jean Navarro <maryjeandnavarro@iskolarngbayan.pup.edu.ph>
2026-08-24 22:51:07 +0800

    Pushed Activity 2 Assignments

---
+++ src/com/joysistvi/stage1/day3/Activity1_Navarro.java
@@ -0,0 +1,25 @@
+package com.joysistvi.stage1.day3;
+
+public class Activity1_Navarro {
+    public static void main (String[] args){
+        boolean amIReal = true;
+        char faveLetter = 'M';
+        byte  faveNumber = 2;
+        short numberOfSVT = 13;
+        int cheolBday = 8081995;
+        long v8Bday = 11079702181998L;
+        float svtDebut = 05.262015F;
+        double caratDay = 02.142016;
+        String svtLeader = "Choi Seungcheol";
+
+        System.out.println("Am I Real: " + amIReal);
+        System.out.println("Fave Letter: " + faveLetter);
+        System.out.println("Fave Number: " + faveNumber);
+        System.out.println("Number Of Seventeen Members: " + numberOfSVT);
+        System.out.println("Scoup's Birthday: " + cheolBday);
+        System.out.println("V8 Birthdays: " + v8Bday);
+        System.out.println("Seventeen's Debut Date: " + svtDebut);
+        System.out.println("Carat's Birthday: " + caratDay);
+        System.out.println("Seventeen Leader: " + svtLeader);
+    }
+}
